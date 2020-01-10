dfd62a6961523c8cc0df9115bcb7143bd0429e33
Kristina Head <kristina_head@icloud.com>
2020-01-10 17:11:16 +0100

    Solution

---
+++ statements-and-arrays/while-and-do-while-loops/2/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    int sum = 0;
+
+    while (number != 0) {
+      sum += number;
+      number = sc.nextInt();
+    }
+
+    System.out.println(sum);
+  }
+}
