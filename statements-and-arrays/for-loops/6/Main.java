f7f2197b825bacd311636f2388a1ce06c1956541
Kristina Head <kristina_head@icloud.com>
2020-01-12 13:28:55 +0100

    Solution

---
+++ statements-and-arrays/for-loops/6/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    for (; a <= b; a++) {
+      if (a % 3 == 0 && a % 5 == 0) {
+        System.out.println("FizzBuzz");
+      } else if (a % 3 == 0) {
+        System.out.println("Fizz");
+      } else if (a % 5 == 0) {
+        System.out.println("Buzz");
+      } else {
+        System.out.println(a);
+      }
+    }
+  }
+}
