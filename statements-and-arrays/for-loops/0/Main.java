e40b688aee0b3e9958b552fd72041e62e4658e14
Kristina Head <kristina_head@icloud.com>
2020-01-12 11:54:02 +0100

    Solution

---
+++ statements-and-arrays/for-loops/0/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = 0;
+
+    for (; a <= b; a++) {
+      sum += a;
+    }
+
+    System.out.println(sum);
+  }
+}
