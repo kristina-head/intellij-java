c65889cd79c97b5ebdd627ccf3a8bd753b39360a
Kristina Head <kristina_head@icloud.com>
2020-02-07 15:37:58 +0100

    Solution using toArray

---
+++ collections/list/3/MainToArray.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainToArray {
+  public static void main(String[] args) {
+    List<GreekLetter> letterList = new ArrayList<>();
+    letterList.add(new GreekLetter("Gamma",  3));
+    letterList.add(new GreekLetter("Omega", 24));
+    letterList.add(new GreekLetter("Alpha",  1));
+
+    Object[] objArray = letterList.toArray(new Object[letterList.size()]);
+
+    for (Object greekLetter : objArray) {
+      System.out.println(greekLetter);
+    }
+  }
+
+  static class GreekLetter {
+    private String letter;
+    private Integer position;
+
+    public GreekLetter(String letter, Integer position) {
+      this.letter = letter;
+      this.position = position;
+    }
+
+    @Override
+    public String toString() {
+      return "{" +
+              "letter='" + letter + '\'' +
+              ", position=" + position +
+              '}';
+    }
+  }
+}
