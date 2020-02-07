fa4006b5691ed1ae8f984fb28085d16c94a5e5c9
Kristina Head <kristina_head@icloud.com>
2020-02-07 15:36:14 +0100

    Solution

---
+++ collections/list/3/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    List<GreekLetter> letterList = new ArrayList<>();
+    letterList.add(new GreekLetter("Gamma",  3));
+    letterList.add(new GreekLetter("Omega", 24));
+    letterList.add(new GreekLetter("Alpha",  1));
+
+    Object[] objArray = new Object[letterList.size()];
+
+    for (int i = 0; i < letterList.size(); i++) {
+      objArray[i] = letterList.get(i);
+    }
+
+    for (Object greekLetter : objArray) {
+      System.out.println(greekLetter);
+    }
+  }
+
+  static class GreekLetter {
+    private String letter;
+    private Integer position;
+
+    public GreekLetter(String letter, Integer position) {
+      this.letter = letter;
+      this.position = position;
+    }
+
+    @Override
+    public String toString() {
+      return "{" +
+              "letter='" + letter + '\'' +
+              ", position=" + position +
+              '}';
+    }
+  }
+}
