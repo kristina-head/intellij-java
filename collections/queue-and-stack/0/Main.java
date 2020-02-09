e74788a951180d1426c800271c705d6a07561570
Kristina Head <kristina_head@icloud.com>
2020-02-09 13:23:30 +0100

    Solution

---
+++ collections/queue-and-stack/0/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayDeque;
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayDeque<Integer> queue = new ArrayDeque<>();
+    queue.add(2);
+    queue.add(0);
+    queue.add(1);
+    queue.add(7);
+
+    System.out.println(queue);
+  }
+}
