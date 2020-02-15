d37a07d326db8d69979374e4e1df10b23d6a6c32
Kristina Head <kristina_head@icloud.com>
2020-02-15 14:07:29 +0100

    Solution

---
+++ collections/map/0/Main.java
@@ -0,0 +1,13 @@
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Main {
+  public static void main(String[] args) {
+    SortedMap<String, Integer> map = new TreeMap<>();
+    map.put("Alpha", 1);
+    map.put("Gamma", 3);
+    map.put("Omega", 24);
+
+    System.out.println(map);
+  }
+}
